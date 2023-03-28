d9f8bfbba8052a4432d5c9aa690ccd67af4f02ed
Mate Kovacs <kovacsut10@gmail.com>
2023-03-28 18:57:28 +0200

    Course app was added - the first part implemented.

---
+++ course-app/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent {
+  ingredients : Ingredient[] = [
+    new Ingredient('cinnamon', 1.0),
+    new Ingredient('apple', 6),
+  ];
+}
