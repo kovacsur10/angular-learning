486398b6966a3c5ab0bf691b8cb9634a1fe87ec2
Mate Kovacs <kovacsut10@gmail.com>
2023-03-28 18:57:52 +0200

    My first app changes.

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: "./servers.component.html",
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!'
+  serverName = 'Test Server';
+  serverCreated = false;
+  servers = ['TestServer', 'TestServer 2']
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName
+    this.servers.push(this.serverName)
+    this.serverCreated = true;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
