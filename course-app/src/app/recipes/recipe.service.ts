c245e9a3052b04e59fb6893d58dd3b2b0b4bbb86
Mate Kovacs <kovacsut10@gmail.com>
2023-04-02 18:24:18 +0200

    Course project added Observables: DONE.

--- course-app/src/app/recipes/recipe.service.ts
+++ course-app/src/app/recipes/recipe.service.ts
@@ -1,12 +1,10 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
-
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a simple test.', 'https://sallysbakingaddiction.com/wp-content/uploads/2013/04/triple-chocolate-cake-4.jpg', [new Ingredient('meat', 2), new Ingredient('salt', 1)]),
     new Recipe('A Test Recipe 2', 'This is a complex test.', 'https://sallysbakingaddiction.com/wp-content/uploads/2013/04/triple-chocolate-cake-3.jpg', [new Ingredient('milk', 1), new Ingredient('eggs', 12)])
